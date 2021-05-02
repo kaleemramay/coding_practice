942cdbb4ba801124984848ed301afda33ab9b24e
kaleemramay <kaleemramay104@gmail.com>
2021-05-02 03:27:16 -0400

    Fixed edge cases for clock hands angle

--- clock_hands_angle/clock_hands_angle.py
+++ clock_hands_angle/clock_hands_angle.py
@@ -2,27 +2,31 @@ import sys
 
 arguments = (sys.argv)
 try:
-    if arguments[1].find(":") == 1:
+    if arguments[1].find(":") > 0:
         time = arguments[1].split(":")
         hour    = int(time[0])
         minute  = int(time[1])
     else:
         hour    = int(arguments[1])
         minute  = int(arguments[2])
+    if minute > 60 or hour > 12:
+        raise Exception
 except Exception as ex:
     print("Invalid format \n Please provide time as \"3:15\" or \"3 15\"")
+    exit
 
 def get_angle(hour,minute):
     if minute == 0:
-        minute_angle = 0
+        min_pos = 0
+        min_bump = 0
     else:    
-        minute_angle = 360/minute
-    if hour == 0:
-        hour_angle = 0
+        min_pos = 360 * (minute / 60)
+        min_bump = (360/12) * (minute/60)
+    if hour == 12 or hour == 0:
+        hour_pos = 0 + min_bump
     else:
-        hour_angle = (360/12) * (hour + (minute/60))
-    angle = abs(hour_angle - minute_angle)
+        hour_pos = (360/12) * hour + min_bump
+    angle = abs(hour_pos - min_pos)
     return angle
-
 angle = get_angle(hour,minute)
 print(angle)
